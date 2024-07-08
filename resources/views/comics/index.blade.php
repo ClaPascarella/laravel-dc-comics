b953c17ff279470d7f347d9c0d44973c788d88b1
ClaPascarella <claudiopascarella1996@gmail.com>
2024-07-08 18:41:15 +0200

    aggiungo href

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 <h1>FUMETTO</h1>
 @foreach ($catalog as $fumetto)
-<p>{{ $fumetto->title}}</p>
+<p>
+    <a href="{{ route("comics.show", $fumetto->id) }}">{{ $fumetto->title}}</a>
+</p>
 @endforeach
 
 @endsection
